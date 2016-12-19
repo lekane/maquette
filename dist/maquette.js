20a0beb17cdab4a9b1adebbba78373fa4a48cd9c
Roman Zaynetdinov <roman.zaynetdinov@lekane.com>
2016-12-19 15:05:50 +0200

    Don't try to return AMD module

--- dist/maquette.js
+++ dist/maquette.js
@@ -1,8 +1,5 @@
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['exports'], factory);
-    } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
+    if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
         // CommonJS
         factory(exports);
     } else {
